d1fdfa01b4746b5b913cc0d176684885fb63afab
sbalci <drserdarbalci@gmail.com>
2018-08-26 22:23:01 +0300

    updated on 2018-08-24 23:34:53

--- GitHubUpdate.R
+++
@@ -1,4 +0,0 @@
-CommitMessage <- paste("updated on ", Sys.time(), sep = "")
-wd <- getwd()
-gitCommand <- paste("cd ", wd, " \n git add . \n git commit --message '", CommitMessage, "' \n git push origin master \n", sep = "")
-system(command = gitCommand, intern = TRUE)
