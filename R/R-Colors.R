29eb2e82fcd09ca3e3480e6f3e1ca39c5392311e
Serdar Balcı <drserdarbalci@gmail.com>
2020-12-13 20:16:07 +0300

    retry

--- R/R-Colors.R
+++
@@ -1,7 +0,0 @@
-# R Colors
-
-# One liner to show all colours available in R
-# https://www.robertmylesmcdonnell.com/content/posts/showcolours/
-
-scales::show_col(colours(), cex_label = .35)
-
--- R/plumberRun.R
+++
@@ -1,4 +0,0 @@
-library(plumber)
-r <- plumb("plumber.R")  # Where 'plumber.R' is the location of the file shown above
-r$run(port=8000)
-
